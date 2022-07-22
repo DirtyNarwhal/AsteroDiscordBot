c180647f74589366f98b0ea30431fa252868fcbc
DirtyNarwhal <mlopez1@hamilton.edu>
2022-07-22 01:20:51 -0500

    Add comments and code cleanup.

--- Python/astero_bot.py
+++ Python/astero_bot.py
@@ -1,15 +1,18 @@
+### ---- Imports and Logger configuration ---- ###
 import discord
-import astero_config
 import logging
+import astero_config
 from discord.ext import commands
-
 print(discord.__version__)
+#Prints logs from Discord into the terminal.
 logging.basicConfig(level=logging.INFO)
 
+### ---- The bot class ---- ###
 class AsteroBot(discord.Client):
+    #Print "ready" when connection with Discord is established.
     async def on_ready(self):
         print("Ready!")
-
+    #Check for messages and respond accordingly.
     async def on_message(self,message):
         if message.author.id == self.user.id:
             return
@@ -18,8 +21,6 @@ class AsteroBot(discord.Client):
 
         if message.content.startswith('congrats'):
             await message.reply('yay!', mention_author=False)
-
-
 
 ### ---- Intents ---- ###
 intents = discord.Intents.default()
